cea34d9252e0ed14b06537e43a1c571d9bcd19f1
ManNguyen <7028401+ManNguyen@users.noreply.github.com>
2020-03-29 22:29:37 +0700

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+//test case
+var x = [0,1.67,2.89];
+var y = [1000,2000,5000,8000,4000];
+var z = [23,"not a num", "erer"];
+
+console.log(
+
+x.LMR(),
+x.LMR(50),
+y.LMR(),
+y.LMR(1000)
+// z.LMR();
+// z.LMR(1000);
+
+);
---
+++ largestRemainderRound.js
@@ -0,0 +1,46 @@
+(function () {
+
+    if (typeof Array.prototype["LMR"] !== "function") {
+        Array.prototype["LMR"] = function (totalSeat) {
+            totalSeat = totalSeat || 100;
+            if (this.length == 0) return [];
+            if (this.find(x => (typeof x !== 'number') || (x < 0))
+
+            ) {
+                throw new Error("This is not an array of positive numbers");
+            }
+
+            var sum = this.reduce((s, i) => s + i, 0);
+
+            var getRemainder = (a) => (a - Math.floor(a)).toFixed(4);
+
+            var result = this.map(function (num, index) {
+                var seats = (num / sum) * totalSeat;
+                return {
+                    floor: Math.floor(seats),
+                    remainder: getRemainder(seats),
+                    index: index
+                };
+            }).sort(function (a, b) {
+                return b.remainder - a.remainder;
+            });
+
+            var lowerSum = result.reduce(function (sum, current) {
+                return sum + current.floor;
+            }, 0);
+
+            var delta = totalSeat - lowerSum;
+            for (var i = 0; i < delta; i++) {
+                result[i].floor++;
+            }
+
+            return result.sort(function (a, b) {
+                return a.index - b.index;
+            }).map(function (result) {
+                return result.floor;
+            });
+        };
+    } else {
+        throw new Error("Array.prototype.LMR(...) already exists");
+    }
+})();
