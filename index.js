fa12c43783a8f639f0cc5af90b6a2c02ccd91d56
ManNguyen <7028401+ManNguyen@users.noreply.github.com>
2020-03-30 10:21:41 +0700

    change example

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
  //test case
 var x = [0,1.67,2.89];
-var y = [1000,2000,5000,8000,4000];
+var y = [1000,1000,5000,1000];
 var z = [23,"not a num", "erer"];
 
 console.log(
 
 x.spread(),
-x.spread(50),
 y.spread(),
+y.spread(50),
+
 y.spread(1000)
 // z.LMR();
 // z.LMR(1000);
