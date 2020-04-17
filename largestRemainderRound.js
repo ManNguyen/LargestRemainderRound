bff45c3132fc120b227a4f3a10849443babf4240
ManNguyen <7028401+ManNguyen@users.noreply.github.com>
2020-04-17 15:09:49 +0700

    add test spec and the decimal function

--- largestRemainderRound.js
+++ largestRemainderRound.js
@@ -1,42 +1,51 @@
 (function () {
-    var getRemainder = (a) => (a - Math.floor(a)).toFixed(4);
+    var getRemainder = (a) => {
+        return (a - Math.floor(a)).toFixed(4)
+    };
+
+    function spread(totalSeats, decimalNum){
+        totalSeats = totalSeats || 100;
+        decimalNum = decimalNum || 0;
+        
+        if (this.length == 0) return [];
+        if (this.find(x => (typeof x !== 'number') || (x < 0))) {
+            throw new Error("This is not an array of positive numbers");
+        }
+
+        if(decimalNum > 0) {
+            var multi = Math.pow(10,decimalNum);
+            //call the spread no decimal against multi of total seats
+            var powSpread = spread.call(this,totalSeats*multi,0);
+            return powSpread.map(e=>e/multi);
+        }
+
+        var sum = this.reduce((s, i) => s + i, 0);
+        //1. distribute default seats and prioritize the party with high remainder
+        var seatDistribution = this.map(function (num, index) {
+            var seats = (num / sum) * totalSeats;
+            return {
+                seats: Math.floor(seats),
+                remainder: getRemainder(seats),
+                index: index
+            };
+        }).sort(function (a, b) {
+            return b.remainder - a.remainder;
+        });
+
+        //2. get the total remain seats
+        var takenSeats = seatDistribution.reduce((sum, current) => (sum + current.seats), 0);
+        var totalRemains = totalSeats - takenSeats;
+
+        // the total remains alway smaller tnan total parties
+        for (var i = 0; i < totalRemains; i++) {
+            seatDistribution[i].seats++;
+        }
+
+        return seatDistribution.sort((a, b) => (a.index - b.index)).map(a => a.seats);
+
+    };
     if (typeof Array.prototype[""] !== "function") {
-        Array.prototype["spread"] = function (totalSeat) {
-            totalSeat = totalSeat || 100;
-            if (this.length == 0) return [];
-            if (this.find(x => (typeof x !== 'number') || (x < 0))
-
-            ) {
-                throw new Error("This is not an array of positive numbers");
-            }
-
-            var sum = this.reduce((s, i) => s + i, 0);
-            var result = this.map(function (num, index) {
-                var seats = (num / sum) * totalSeat;
-                return {
-                    floor: Math.floor(seats),
-                    remainder: getRemainder(seats),
-                    index: index
-                };
-            }).sort(function (a, b) {
-                return b.remainder - a.remainder;
-            });
-
-            var lowerSum = result.reduce(function (sum, current) {
-                return sum + current.floor;
-            }, 0);
-
-            var delta = totalSeat - lowerSum;
-            for (var i = 0; i < delta; i++) {
-                result[i].floor++;
-            }
-
-            return result.sort(function (a, b) {
-                return a.index - b.index;
-            }).map(function (result) {
-                return result.floor;
-            });
-        };
+        Array.prototype["spread"] = spread;
     } else {
         throw new Error("Array.prototype.spread(...) already exists");
     }
---
+++ spec/testSpec.js
@@ -0,0 +1,38 @@
+require('../largestRemainderRound.js');
+describe("A suite", function () {
+  console.log("Spec Run Correctly");
+  it("test test", function () {
+
+    expect(true).toBe(true);
+  });
+});
+
+describe("Testing the Array.spread function", function () {
+
+  console.log("n");
+  var arr = [1000, 1000, 5000, 1000];
+  
+  it("default spread to 100", function () {
+    expect(arr.spread()).toEqual([13, 13, 62, 12]);
+  });
+
+  it("spread correct to 50", function () {
+    expect(arr.spread(50)).toEqual([7, 6, 31, 6]);
+  });
+
+  it("spread correct to 1000", function () {
+    expect(arr.spread(1000)).toEqual([125, 125, 625, 125]);
+  });
+
+  var arrB = arr.spread(1000).map(e => e/10);
+  var arrC = arr.spread(10000).map(e => e/100);
+
+  it("spread correct to decimal 1", function () {
+    expect(arr.spread(100,1)).toEqual(arrB);
+  });
+
+  it("spread correct to decimal 2", function () {
+    expect(arr.spread(100,2)).toEqual(arrC);
+  });
+
+});
